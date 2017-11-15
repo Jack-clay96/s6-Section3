6b555512884d6ae4580915601a1138839fb930d5
Jack-clay96 <32362918+Jack-clay96@users.noreply.github.com>
2017-11-15 19:10:45 +0000

    update

--- www/js/index.js
+++ www/js/index.js
@@ -1,15 +1,79 @@
-$(document).on("pagecreate","#pageone",function(){
-  $('#submitButton').on("click", function(){
-    submitText();
-  });            
-});            
+var value = "nexus";
+var key = "phone";
+var value1 = "wallet";
+var key1 = "black";
+//var glasses = "green";
+//var drink = "coke";
+//var food = "burger";
+
+//JSON:
+var choc= {
+    "name" : "twix",
+    "weight" : "100g",
+    "carbs" : "20g"
+};
+
+var chocList = [{
+    "name": "Twirl",
+    "quality": "Good"},
+                
+    {"name": "Flake",
+     "quality": "Average"}];
+        
 
 
-function submitText() {
-	var text = $('#textinput').val();
-	alert(text);
+
+window.localStorage.setItem( key, value);
+
+
+document.addEventListener("deviceready", onDeviceReady, false);
+//localStorage2();
+
+
+
+//$ is used in jquery. # = ID. $ is looking at the launched in HTML document. 	
+function updateDisplay() {
+	$("#launched").text("Application launched: " + launched_count);
 }
 
-function storeValue(key, value) {
-	//add some code to store the key-value pair in persistant storage 
+
+// device APIs are available
+//
+    function onDeviceReady() {
+	
+	updateDisplay();  
+	alert("device ready");
+    //localStorage2(); //best to call methods here. As it waits until phone is ready. (Cant test on webpage from here) 
+    }
+
+
+
+    function localStorage2() //Function name can't be something used for something else
+    {
+        window.localStorage.setItem( key, value );
+        var getPhone = window.localStorage.getItem(key);
+        alert("Phone: " + getPhone);
+        
+         window.localStorage.setItem( key1, value1 );
+        var getPhone = window.localStorage.getItem(key1);
+        $("#key1").text("Wallet: " + key1); //#key1 is being replaced by wallet - Used for positioning on page within html document
+		
+        var items = window.localStorage.length;
+        console.log("No. item stored: " + items);
+        
+    }
+
+function jsonString()
+{
+    var myJSON = JSON.stringify(choc); //turn the var choc into a string
+    console.log("my favorite chocolate: " + choc.name + " and  weight is: " + choc.weight);
+}
+
+function jsonList()
+{
+    var output = '';
+    $.each(chocList, function(index, value){
+           output += '<li>' +value.name + '</li>';
+           });
+    $('#searchChoc').html(output).listview("refresh");
 }
